8fbd1ea26cd8c40b4eba99760171f8ba4d774afc
Diego Longhi <diego@ibservi.com.br>
2021-07-12 12:09:00 -0300

    Add project files.

---
+++ src/Diego.Redis.WebApi/Context/ApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diego.Redis.WebApi.Context
+{
+    public class ApiDbContext : DbContext
+    {
+    }
+}
